c142354e05ef2fcb9198ed21046c8d588110acc8
cow-co <jc13988@my.bristol.ac.uk>
2016-12-28 15:05:56 +0000

    Start Welcome Page

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+#Load Amazon Web Services settings from config file.
+AWS_SETTINGS = YAML.load_file("#{Rails.root}/config/aws_dynamo.yml")
+
 # Initialize the Rails application.
 Rails.application.initialize!
