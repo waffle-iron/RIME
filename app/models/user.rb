24b6e4937f53e9cc86c29d92e7d6f174bd909db6
cowco <jc13988@my.bristol.ac.uk>
2017-01-03 16:13:06 +0000

    Add Password to User Model
    Need to do some stuff on my Linux side to
    finish this password thing off (db migrations etc.)
    
    Signed-off-by: cowco <jc13988@my.bristol.ac.uk>

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,6 @@ class User < ApplicationRecord
     validates :name, presence: true, length: {maximum: 52}, uniqueness: true
     validates :email, presence: true, length: {maximum: 255},
                 format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+    has_secure_password
+    validates :password, presence: true, length: {minimum: 6}
 end
