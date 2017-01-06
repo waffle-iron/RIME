9113b69a2330b4a5130245ac59579695ad343f4c
cow-co <jc13988@my.bristol.ac.uk>
2017-01-06 09:23:39 +0000

    Spruce up CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#welcome'
+
+  get '/signup', to: 'users#new'
+  get 'welcome/welcome'
 end
