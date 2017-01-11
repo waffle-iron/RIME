3ad69b3788559a8fa2d663c49f76bfa451b11b8f
cow-co <jc13988@my.bristol.ac.uk>
2017-01-11 18:26:09 +0000

    Begin to Implement Email Verification

---
+++ app/controllers/accout_activations_controller.rb
@@ -0,0 +1,2 @@
+class AccoutActivationsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get 'users/signup_success'
-  
+
   get 'welcome/welcome'
 
   resources :users
+  resources: :account_activations, only: [:edit]    #Only providing an Edit route for this resource. 
 end
