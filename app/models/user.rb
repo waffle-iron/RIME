8e9fa00aa8099d5955bd3d795e88733d0d9fee3b
cow-co <jc13988@my.bristol.ac.uk>
2017-01-13 18:16:21 +0000

    Finish Password Reset
    Just need to sort out some config.

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,6 +67,10 @@ class User < ApplicationRecord
       UserMailer.password_reset(self).deliver_now
     end
 
+    def password_reset_expired?
+      reset_at < 2.hours.ago
+    end
+
     private
         #For email activation
         def create_activation_digest
